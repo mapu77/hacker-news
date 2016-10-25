982b586a67337802ab2e3dc3dba689742ed5d34d
Eduard Maura <edu.maura.puig@gmail.com>
2016-10-25 17:39:41 +0000

    Users Created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
+  root 'controller_name#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
