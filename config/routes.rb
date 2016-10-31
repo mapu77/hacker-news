7c573946d57e26d8a281606efbc9efe6ce2fcdbb
Eduard Maura <edu.maura.puig@gmail.com>
2016-10-31 10:48:44 +0000

    prova routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :contributions
   resources :users
   
-  get 'users' => 'users#index'
+  get '/users' => 'users#index'
   
   root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
