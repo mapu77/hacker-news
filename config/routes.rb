837d0aea3095bf4effe5c6b270ffbb655c866631
Eduard Maura <edu.maura.puig@gmail.com>
2016-10-31 09:55:24 +0000

    proves routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :contributions
   resources :users
-  root 'contributions#index'
+  root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
