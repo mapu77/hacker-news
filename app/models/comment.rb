1a99b011fb8946af7afefae6476ba246777a6d78
Arnau Orriols <aog182@gmail.com>
2016-11-15 12:01:25 +0000

    arreglar el problema amb els blank forms

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,5 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :contribution
     has_many :replies
-    before_save :convert_blank_to_nil
-    validate :presenceContent
-
-  def presenceContent
-    if (content.blank?)
-      errors[:base] << "A comment needs to be provided"
-    end
-  end
     
 end
